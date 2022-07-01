1d2e544db7e3b0be35505abe0b138826828fb4f2
chris <65360545+chrisortega1928@users.noreply.github.com>
2022-07-01 13:57:23 -0400

    changes

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -11,7 +11,7 @@ const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: 'white',
+        backgroundColor: 'gray',
         width:'100%',
 
         borderColor: '#ffac31',
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,19 +1,41 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, Pressable, Alert, Image, useWindowDimensions } from 'react-native'
 import React, {useState} from 'react'
 import CustomInput from '../../components/CustomInput/CustomInput'
 import axios from 'axios';
 import CustomButton from '../../components/CustomButton'
 import ImagePicker from 'react-native-image-crop-picker';
+import Camera from '../../../assets/images/camera-icon.png'
+import Upload from '../../../assets/images/upload-icon.png'
 
 const Index = () => {
-  // const takeVid = () =>{
-  //   ImagePicker.openCamera({
-  //     mediaType: 'video',
-  //   }).then(image => {
-  //     console.log(image);
-  //   });
-  // }
+  const takeVid = () =>{
+    ImagePicker.openCamera({
+      mediaType: 'video',
+    }).then(image => {
+      
+      console.log(image);
+    }).catch((err) => { console.log("openCamera catch " + err.toString()) });
+  }
 
+  const pickVid = () =>{
+    ImagePicker.openPicker({
+      mediaType: "video",
+    }).then((video) => {
+      console.log(video);
+    }).catch((err) => { console.log("openPicker catch " + err.toString()) });
+  }
+
+  const takePic = () => {
+    ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+    }).catch((err) => { console.log("openPicker catch " + err.toString()) });
+  }
+
+  const {height} = useWindowDimensions();
 
   
   const [title, setTitle] = useState('');
@@ -43,88 +65,25 @@ const Index = () => {
   })
 }
 
-
-//     axios.post('http://10.0.2.2:1337/api/stories', JSON.stringify({
-//     "data": {
-//       "title": "setTitle",
-//       "description": "setDescription",
-//       "tag": "setTag",
-//       } 
-//   }), {
-//     headers: {
-//         "Content-Type": "application/json",
-//     }
-// }).then((response) => {
-//      console.log("reactNativeDemo","response get details:" + response.data);
-//   })
-//   .catch((error) => {
-//      console.log(error.response);
-//   });
-
-
-  //   await axios.get(
-  //     'http://10.0.2.2:1337/api/stories',
-  //     {
-  //       data: {
-  //         title: 'setTitle',
-  //         description: 'setDescription',
-  //         tag: 'setTag',
-  //       }
-  //   }
-  //   )
-  //   .then((response) => {
-  //     console.warn('success')
-  // }).catch((error) => {
-  //     console.warn(setTag)
-  // })
-
-  // fetch("http://localhost:1337/api/stories", {
-  //   method: "POST", // *GET, POST, PUT, DELETE, etc.
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   },
-  //   body: JSON.stringify({
-  //     "data":{
-  //       "title":"csc101",
-  //       "description":"this is csc101",
-  //       "tag":"T"
-  //   }
-  //   }) 
-  // })
-  //   .then(res => {
-  //     res.json();
-  //   })
-  //   .then(data => console.log(data))  // ur data is here
-  //   .catch(err => console.log("api Erorr: ", err));
-
-
-//   const onSubmit = () => {
-//   fetch('http://10.0.2.2:1337/api/stories', {
-//   method: 'GET',
-//   headers: {
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     data: {
-//       title: 'setTitle',
-//       description: 'setDescription',
-//       tag: 'setTag',
-//       }
-//   })
-// });
-// }
-
   return (
     <View style={styles.body}>
-      {/* <Text>Camera</Text>
-        <Button
-        onPress={takeVid} title='Cam'
-        /> */}
+      
+      <Pressable onPress={takePic}>
+        <Image
+          source={Camera} style={[styles.camera, {height: height*0.15}]} resizeMode='contain'
+        />
+      </Pressable>
+      <Pressable onPress={pickVid}>
+        <Image
+          source={Upload} style={[styles.upload, {height: height*0.15}]} resizeMode='contain'
+         />
+      </Pressable>
+
       <CustomInput placeholder="title" value={title} setValue={setTitle}/>
       <CustomInput placeholder="description" value={description} setValue={setDescription}/>
       <CustomInput placeholder="tag" value={tag} setValue={setTag}/>
-      <CustomButton text="Submit" onPress={onSubmit}/>   
+      <CustomButton text="Submit" onPress={onSubmit}/>
+        
     </View>
   );
 }
@@ -136,9 +95,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  cam: {
+  camera: {
     width:100,
     height: 100,
+    backgroundColor: '#ededed',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+  upload:{
+    width:100,
+    height: 100,
+    backgroundColor: '#ededed',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'black',
   }
 });
 
